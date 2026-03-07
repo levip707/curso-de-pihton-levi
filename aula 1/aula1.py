f4cc25453cca8cf6bc3797497b49d30a6bb0695f
trabalho01-python <trabalho74549python@gmail.com>
2026-03-07 13:03:48 -0300

    first commit

---
+++ aula 1/aula1.py
@@ -0,0 +1,122 @@
+# print('Olá mundo!')
+
+# #aqui falaremos sobre variáveis
+
+# numero = 10
+
+# print(numero)
+
+# Nome = 'Levi'
+
+# print(Nome)
+
+# Idade = 19
+
+# print(Idade)
+
+# # Hardecode
+
+# x = 10
+# y = 2
+
+# #soma
+# soma = x + y
+# print('soma = ',soma)
+
+# #subtração
+
+# subtracao = x - y
+# print('subtracao = ',subtracao)
+
+# #multiplicaçao
+# multiplicacao = x * y
+# print('multiplicacao = ',multiplicacao)
+
+# #divisão
+# divisao = x/y
+# print('divisao = ',divisao)
+
+# #resto da divisão
+# resto = x % y
+# print('o resto é',resto)
+
+# #potencia
+# potencia = x ** y
+# print(x,'elevado a',y,' = ',potencia)
+
+# #Dividão por inteiro
+# divisaoporInteiro = x//y
+# print(divisaoInteiro)
+
+# #operadores de atribuição
+# # =
+# a = 2
+# # +=
+# a += 3
+# a -= 1
+# a *= 2
+# a /= 4
+# a **= 2
+# a //= 3
+
+# #Operadores Relacionais
+
+# a = 10
+# b = 5
+
+# print(a > b) # True
+# print(a < b) # False
+# print(a >= b) # True
+# print(a <= b) # False
+# print(a == b) # False
+# print(a != b) # True
+
+# sol = True
+# dinheiro = False
+# print('Praia (and):', sol and dinheiro)
+# print('Praia (or): ', sol or dinheiro)
+
+# a = 10 > 2
+
+#  print(not a)
+
+# #potencias e raizes
+# #multiplicações e divisões
+# #soma e subtração
+
+# a = (10 + 2) * 3
+
+# print(a)
+# n1 = 10
+# n2 = 8
+# n3 = 6
+
+# media = (n1 + n2 + n3) /3
+
+# print (media)
+
+#entrada de dados
+
+#nome = input(' Digite o seu nome: ')
+#print ( Olá ´,nome)
+
+x = input('Digiti o primeiro número:')
+y = input('Digiti o primeiro número:')
+
+# casting
+x = int(x)
+y = int(y)
+
+print ('O resultado da soma é ' , (x + y) )
+
+# Escreva um progama em python que receba do usuário 2 números e mostre o resultado da soma, subtração, divisão e multiplicação desses números.
+
+print ('O resultado da soma é ' , (x + y) )
+print ('O resultado da subtração é ' , (x + y) )
+
+    
+
+
+      
+
+      
